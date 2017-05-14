0365ffdf22ffa847e81618f34746ea43b6ebaeb4
Gabriel Mukobi <gabrielmukobi@gmail.com>
2017-05-13 21:23:59 -0700

    Add responsive navigation and (somewhat) jumbotron

--- js/jumbotron.js
+++ js/jumbotron.js
@@ -1,15 +1,17 @@
 
+w=window,d=document,e=d.documentElement,g=d.getElementsByTagName('body')[0],x=w.innerWidth||e.clientWidth||g.clientWidth,y=w.innerHeight||e.clientHeight||g.clientHeight;
 
 var scrolled = false;
 var stuckPos;
+var magicStopPoint = .75*x - .54*y - 74;
 
 window.onscroll = function() {
   var txt = document.getElementById('jumbo_txt_container');
   var topPos = txt.getBoundingClientRect().top;
 
-  if ( window.pageYOffset > 600) {
+  if ( window.pageYOffset > magicStopPoint) {
     if (scrolled == false) {
-      stuckPos = (topPos + 600) + "px";
+      stuckPos = (topPos + magicStopPoint) + "px";
       scrolled = true;
     }
     txt.classList.add('scrolled_down');
@@ -18,4 +20,5 @@ window.onscroll = function() {
     txt.classList.remove('scrolled_down');
     txt.style.top = "50%";
   }
+  console.log("X: " + x + " Y: " + y + " yOff: " + window.pageYOffset);
 }
