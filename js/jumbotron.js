3729490532e569f8549225cd7d1c73fcddf5af89
Gabriel Mukobi <gabrielmukobi@gmail.com>
2017-05-14 15:14:39 -0700

    Responsive jumbotext scrolling js

--- js/jumbotron.js
+++ js/jumbotron.js
@@ -1,24 +1,45 @@
 
-w=window,d=document,e=d.documentElement,g=d.getElementsByTagName('body')[0],x=w.innerWidth||e.clientWidth||g.clientWidth,y=w.innerHeight||e.clientHeight||g.clientHeight;
+window.addEventListener('load',
+  function() {
 
-var scrolled = false;
-var stuckPos;
-var magicStopPoint = .75*x - .54*y - 74; //equation to convert window height and width to value used to determine how far down the jumbotron text box should stop scrolling
+  w=window,d=document,e=d.documentElement,g=d.getElementsByTagName('body')[0],x=w.innerWidth||e.clientWidth||g.clientWidth,y=w.innerHeight||e.clientHeight||g.clientHeight;
 
-window.onscroll = function() {
-  var txt = document.getElementById('jumbo_txt_container');
-  var topPos = txt.getBoundingClientRect().top;
 
-  if ( window.pageYOffset > magicStopPoint) {
-    if (scrolled == false) {
-      stuckPos = (topPos + magicStopPoint) + "px";
-      scrolled = true;
+
+  if (x > 960) {
+    var scrolled = false;
+    var stuckPos;
+    var magicStopPoint = .75*x - .54*y - 74; //equation to convert window height and width to value used to determine how far down the jumbotron text box should stop scrolling
+
+    function jumboTextScroller() { //logic for freezing jumbotron text
+      var txt = document.getElementById('jumbo_txt_container');
+      var topPos = txt.getBoundingClientRect().top;
+
+      if ( window.pageYOffset > magicStopPoint) {
+        if (scrolled == false) {
+          stuckPos = (topPos + magicStopPoint) + "px";
+          scrolled = true;
+        }
+        txt.classList.add('scrolled_down');
+        txt.style.top = stuckPos;
+      } else {
+        txt.classList.remove('scrolled_down');
+        txt.style.top = "50%";
+      }
     }
-    txt.classList.add('scrolled_down');
-    txt.style.top = stuckPos;
-  } else {
-    txt.classList.remove('scrolled_down');
-    txt.style.top = "50%";
+
+
+    jumboTextScroller(); //initial calling when page loads
+
+
+    window.onscroll = function() {
+      jumboTextScroller();
+    }
   }
-  console.log("X: " + x + " Y: " + y + " yOff: " + window.pageYOffset);
-}
+
+
+
+
+
+
+}, false);
