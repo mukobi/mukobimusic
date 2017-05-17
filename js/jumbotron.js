630ef978918aa9a4e8009da5e156d9a937a83ec8
Gabriel Mukobi <gabrielmukobi@gmail.com>
2017-05-16 18:59:10 -0700

    Simplify js and make work with scrolled down refresh

--- js/jumbotron.js
+++ js/jumbotron.js
@@ -1,18 +1,18 @@
-function jumboTextAlign() {
+window.addEventListener('load', function() {
   w=window,d=document,e=d.documentElement,g=d.getElementsByTagName('body')[0],x=w.innerWidth||e.clientWidth||g.clientWidth,y=w.innerHeight||e.clientHeight||g.clientHeight;
 
   if (x > 960) { //desktop
     var scrolled = false;
     var stuckPos;
-    var magicStopPoint = .75*x - .54*y - 74; //equation to convert window height and width to value used to determine how far down the jumbotron text box should stop scrolling
+    var magicStopPoint = .75*x - .53*y - 60; //equation to convert window height and width to value used to determine how far down the jumbotron text box should stop scrolling
 
     function jumboTextScroller() { //logic for freezing jumbotron text
       var txt = document.getElementById('jumbo_txt_container');
       var topPos = txt.getBoundingClientRect().top;
-
-      if ( window.pageYOffset > magicStopPoint) {
+      console.log("TP " + topPos + " magicStopPoint " + magicStopPoint + " x " + x + " y " + y);
+      if (window.pageYOffset > magicStopPoint) {
         if (scrolled == false) {
-          stuckPos = (topPos + magicStopPoint) + "px";
+          stuckPos = (.5 * y - 95 + magicStopPoint) + "px";
           scrolled = true;
         }
         txt.classList.add('scrolled_down');
@@ -37,10 +37,6 @@ function jumboTextAlign() {
     txt.style.position = "absolute";
     txt.style.transform = "translate(-50%,-50%)";
   }
-}
 
-
-window.addEventListener('load', function() {
-  jumboTextAlign(); //initial call on page load
-  window.addEventListener('resize', jumboTextAlign); //further calls when viewport is resized
+  // window.addEventListener('resize', jumboTextAlign); //further calls when viewport is resized
 }, false);
